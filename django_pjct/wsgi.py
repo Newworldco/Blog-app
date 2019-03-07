5b516e57ccfe2176edebf5fa008eee2842505b6c
Newworldco <taishuaihu@gmail.com>
2019-03-07 18:54:32 +0800

    修改wsgi

--- django_pjct/wsgi.py
+++ django_pjct/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
+import sys
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_pjct.settings')
+path = "/home/Invictuss/django_pjct"
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_pjct.settings'
 
 application = get_wsgi_application()
